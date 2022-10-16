920c1c6b8bef7ddc901c14b92caef918108d019a
Vadim Zaytsev <zaic101@gmail.com>
2022-10-16 17:39:24 +0400

    Add base class for GUI part

---
+++ src/task-processor/public/TaskProcessor/TestVisualizerBase.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <QObject>
+
+class TestVisualizerBase : public QObject {
+    Q_OBJECT
+
+public:
+    virtual ~TestVisualizerBase() = default;
+
+    virtual void drawVisualizationInto(QWidget& parent) = 0;
+
+public slots:
+    virtual void resetTestContent(QString input, QString answer) = 0;
+
+signals:
+    void inputChanged(QString input);
+    void answerChanged(QString answer);
+};
